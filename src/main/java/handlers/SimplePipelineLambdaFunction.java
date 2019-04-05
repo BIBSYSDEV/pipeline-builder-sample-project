47c24805d2aca43d26ab62bedc97a92f3915cdd4
Orestis Gkorgkas <og@bibsys.no>
2019-04-05 12:13:42 +0200

    write tests

--- src/main/java/handlers/SimplePipelineLambdaFunction.java
+++ src/main/java/handlers/SimplePipelineLambdaFunction.java
@@ -8,24 +8,34 @@ import no.bibsys.aws.lambda.handlers.templates.CodePipelineFunctionHandlerTempla
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SimplePipelineLambdaFunction extends CodePipelineFunctionHandlerTemplate<String> {
+public class SimplePipelineLambdaFunction extends CodePipelineFunctionHandlerTemplate<OutputObject> {
 
     private static final Logger logger = LoggerFactory.getLogger(SimplePipelineLambdaFunction.class);
     private static final String NULL_INPUT_OBJECT = "null input Object";
     private static final String NULL_INPUT_REQUEST = "null input request";
 
+    /**
+     * Called by AWS/
+     */
     public SimplePipelineLambdaFunction() {
         super(new CodePipelineCommunicator());
     }
 
+
+    protected SimplePipelineLambdaFunction(CodePipelineCommunicator communicator) {
+        super(communicator);
+    }
+
     @Override
-    protected String processInput(DeployEvent inputObject, String inputRequest, Context context) {
+    protected OutputObject processInput(DeployEvent inputObject, String inputRequest, Context context) {
         String inputObjectString = Optional.ofNullable(inputObject).map(Object::toString)
             .orElse(NULL_INPUT_OBJECT);
         String inputRequestString = Optional.ofNullable(inputRequest).orElse(NULL_INPUT_REQUEST);
 
-        String output = String.format("%s %s", inputObjectString, inputRequestString);
-        logger.debug(output);
+        String message = String.format("%s %s", inputObjectString, inputRequestString);
+        OutputObject output= new OutputObject();
+        output.setMessage(message);
+        logger.debug(output.getMessage());
         return output;
     }
 }
